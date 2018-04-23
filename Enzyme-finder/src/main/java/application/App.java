d5efcc7cb1709bd51bea2808e90b7bef7d54c114
geffe <millalinnea.keranen@gmail.com>
2018-04-23 16:28:45 +0300

    database sekoilua

---
+++ Enzyme-finder/src/main/java/application/App.java
@@ -0,0 +1,56 @@
+
+package application;
+
+import com.mycompany.enzyme.finder.Enzyme;
+import com.mycompany.enzyme.finder.EnzymeList;
+import com.mycompany.enzyme.finder.Sequence;
+import com.mycompany.enzyme.finder.SequenceSplitter;
+import database.DatabaseConnection;
+import java.util.Scanner;
+
+
+public class App {
+    
+    public void run(){
+        DatabaseConnection foobar = new DatabaseConnection();
+        
+        foobar.createNewDatabase("test2.db");
+        EnzymeList enzymes = new EnzymeList();
+
+        enzymes.addEnzyme(new Enzyme("EcoRI", "GAATTC"));
+        enzymes.addEnzyme(new Enzyme("BamHI", "GGATCC"));
+
+        Scanner lukija = new Scanner(System.in);
+        System.out.println("Syötä sekvenssi: ");
+        Sequence input = new Sequence("ACTGGAATTCGCTACGGAATTCGATCGATCGAGGATCCCTGTGCAGAATTCCTGAT");
+        //                                 -EcoRI-     -EcoRI-         -BamHI-      -EcoRI- 
+
+        System.out.println("Entsyymit: ");
+        System.out.println(enzymes);
+
+        EnzymeList valitut = new EnzymeList();
+
+        while (true) {
+            System.out.println("Syötä halutun entsyymin nimi tai lopeta");
+            String syote = lukija.nextLine();
+
+            if (syote.equals("lopeta")) {
+                break;
+            } else if (enzymes.searchName(syote)) {
+                valitut.addEnzyme(enzymes.getEnzyme(syote));
+            } else if (syote.isEmpty() && valitut.getEnzymes().isEmpty()) {
+                valitut.addEnzyme(enzymes.getEnzyme("BamHI"));
+                System.out.println("Käytetään oletusentsyymiä BamHI");
+                break;
+            } else {
+                System.out.println("Käytä parempia arvoja tyhmä!");
+            }
+        }
+
+        System.out.println(input.getSequence());
+        SequenceSplitter splitter = new SequenceSplitter(valitut);
+        splitter.setSequence(input.getSequence());
+        splitter.split(input.getSequence());
+        splitter.editSequence();
+    }
+}
--- Enzyme-finder/src/main/java/database/DatabaseConnection.java
+++ Enzyme-finder/src/main/java/database/DatabaseConnection.java
@@ -2,13 +2,21 @@ package database;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
  
 /**
  *
  * @author sqlitetutorial.net
  */
 public class DatabaseConnection {
+    
+    private String url;
+    
+    public DatabaseConnection(){
+        this.url = null;
+    }
  
     /**
      * Connect to a sample database
@@ -17,13 +25,46 @@ public class DatabaseConnection {
      */
     public void createNewDatabase(String fileName) {
  
-        String url = "jdbc:sqlite:/tmp/" + fileName;
+        this.url = "jdbc:sqlite:/tmp/" + fileName;
  
         try (Connection conn = DriverManager.getConnection(url)) {
             if (conn != null) {
                 DatabaseMetaData meta = conn.getMetaData();
                 System.out.println("The driver name is " + meta.getDriverName());
                 System.out.println("A new database has been created.");
+                Statement stmt = conn.createStatement();
+                ResultSet tables = meta.getTables(null, null, "Enzymes", null);
+                
+                if(!tables.next()){
+                    String sql = "CREATE TABLE Enzymes"+
+                                "(NAME varchar(10) PRIMARY KEY NOT NULL,"+
+                                "SEQUENCE varchar(10) NOT NULL)";
+                
+                stmt.executeUpdate(sql);
+                System.out.println("Taulu luotu");
+                }
+                
+                
+            }
+ 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void setDefaultEnzymes(){
+        try (Connection conn = DriverManager.getConnection(url)) {
+            System.out.println("luotu yhteys");
+            if (conn != null) {
+                
+                Statement stmt = conn.createStatement();
+                
+                String sql = "INSERT INTO Enzymes"+
+                                "(NAME,SEQUENCE)"+
+                                "(AatII,GACGTC)";
+                stmt.executeUpdate(sql);
+                System.out.println("lisätty entsyymi");
+                
             }
  
         } catch (SQLException e) {
